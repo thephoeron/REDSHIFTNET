8e4533cd988f7a52b5aa32477dd40d38f3a17151
the Phoeron <thephoeron@me.com>
2013-11-04 01:22:23 -0500

    Removed top-level db connection due to regular timeouts, update admin ui, add additional admin pages

--- admin/admin.lisp
+++ admin/admin.lisp
@@ -63,11 +63,13 @@
 (defun admin-footer ()
   "Template block for admin site footer."
   (cl-who:with-html-output (hunchentoot::*standard-output*)
-    (:div :class "push")
-    (:div :class "container"
+    (:div :class "push" :style "display: inline-block; height: 100px; position: relative;" "&nbsp;")
+    (:div :class "container-fluid" :style "position: fixed; bottom: 0; height: 80px; width: 100%; padding: 15px 0; background-color: rgba(238,238,238,0.5); border-top: 1px solid rgba(85,85,85,0.5);"
       (:div :class "row"
-        (:div :class "col-md-12"
-          (:p :class "muted credit"
+        (:div :class "col-lg-12"
+          (:p :class "muted credit center"
+            (fmt "Crafted in (~C) Common Lisp" #\GREEK_SMALL_LETTER_LAMDA)
+            (:br)
             (fmt "Powered by ~A v~A and ~A v~A."
               (lisp-implementation-type)
               (lisp-implementation-version)
@@ -85,6 +87,12 @@
           (:li (:a :href "/admin/dashboard/"
             (:span :class "icon" (:i :class "icon20 i-screen"))
             (:span :class "txt" "Dashboard")))
+          (:li (:a :href "/admin/webmail/"
+            (:span :class "icon" (:i :class "icon20 i-envelop-2"))
+            (:span :class "txt" "Webmail")))
+          (:li (:a :href "/admin/calendar/"
+            (:span :class "icon" (:i :class "icon20 i-calendar"))
+            (:span :class "txt" "Calendar")))
           (:li (:a :href "#tables"
             (:span :class "icon" (:i :class "icon20 i-table-2"))
             (:span :class "txt" "Tables")
@@ -94,7 +102,33 @@
                        (:span :class "txt" "All Tables")))
               (:li (:a :href "/admin/tables/person/"
                        (:span :class "icon" (:i :class "icon20 i-table"))
-                       (:span :class "txt" "Person Table")))))))))))
+                       (:span :class "txt" "Person Table"))))))
+          (:li (:a :href "#settings"
+            (:span :class "icon" (:i :class "icon20 i-cogs"))
+            (:span :class "txt" "Settings")
+            (:ul :class "sub"
+              (:li (:a :href "#"
+                      (:span :class "icon" (:i :class "icon20 i-cogs"))
+                      (:span :class "txt" "All Settings")))
+              (:li (:a :href "#"
+                      (:span :class "icon" (:i :class "icon20 i-cube-3"))
+                      (:span :class "txt" "Widgets"))))))
+          (:li (:a :href "#about"
+            (:span :class "icon" (:i :class "icon20 i-notification"))
+            (:span :class "txt" "About")
+            (:ul :class "sub"
+              ;; credits
+              (:li (:a :href "#"
+                (:span :class "icon" (:i :class "icon20 i-quill-3"))
+                (:span :class "txt" "Credits")))
+              ;; documentation
+              (:li (:a :href "#"
+                (:span :class "icon" (:i :class "icon20 i-stack-list"))
+                (:span :class "txt" "Documentation")))
+              ;; help
+              (:li (:a :href "#"
+                (:span :class "icon" (:i :class "icon20 i-question"))
+                (:span :class "txt" "Help")))))))))))
 
 ;; Admin Login page
 (eval-when (:compile-toplevel :execute :load-toplevel)
@@ -107,14 +141,24 @@
   ;; Admin Dashboard
   (defparameter admin-dashboard-styles
     (list "/static/js/plugins/tables/datatables/jquery.dataTables.css"
-          "/static/css/custom.css"
-          "/static/css/app.css"))
+          "/static/css/app.css"
+          "/static/css/custom.css"))
   (defparameter admin-dashboard-scripts 
     (list "/static/js/plugins/forms/uniform/jquery.uniform.min.js"
           "/static/js/plugins/tables/datatables/jquery.dataTables.min.js"
           "/static/js/pages/data-tables.js"
-          "/static/js/pages/dashboard.js"
-          "/static/js/app.js")))
+          "/static/js/app.js"
+          "/static/js/pages/dashboard.js"))
+  ;; Admin Calendar
+  (defparameter admin-calendar-styles
+    (list "/static/js/plugins/misc/fullcalendar/fullcalendar.css"
+          "/static/css/app.css"
+          "/static/css/custom.css"))
+  (defparameter admin-calendar-scripts
+    (list "/static/js/plugins/forms/uniform/jquery.uniform.min.js"
+          "/static/js/plugins/misc/fullcalendar/fullcalendar.min.js"
+          "/static/js/app.js"
+          "/static/js/pages/calendar.js")))
 
 (define-rsn-form (admin-login-form :submit "Login" :action "")
   ((username text
@@ -182,7 +226,6 @@
         (:link :rel "stylesheet" :href "/static/css/bootstrap/bootstrap.min.css" :type "text/css" :media "screen")
         (:link :rel "stylesheet" :type "text/css" :href "/static/css/bootstrap/bootstrap-theme.css")
         (:link :rel "stylesheet" :type "text/css" :href "/static/css/icons.css")
-        (:link :rel "stylesheet" :type "text/css" :href "/static/js/plugins/forms/uniform/uniform.default.css")
         (:link :rel "stylesheet" :type "text/css" :href "/static/js/plugins/ui/jgrowl/jquery.jgrowl.css")
         "<!--[if IE 8]><link href=\"css/ie8.css\" rel=\"stylesheet\" type=\"text/css\" /><![endif]-->"
         (:link :rel "apple-touch-icon-precomposed" :sizes "144x144" :href "/static/images/ico/apple-touch-icon-144-precomposed.png")
@@ -191,7 +234,6 @@
         (:link :rel "apple-touch-icon-precomposed" :href "/static/images/ico/apple-touch-icon-57-precomposed.png")
         (:link :rel "shortcut icon" :href "/static/images/ico/favicon.png")
         ;(:link :rel "stylesheet" :href "/redshiftnet.css" :type "text/css" :media "screen")
-        (:link :rel "stylesheet" :href "/static/css/app.css" :type "text/css" :media "screen")
         ,@(mapcar (lambda (file)
                     `(:link :type "text/css" :rel "stylesheet" :media "screen"
                             :href ,(format nil "~A" file)))
@@ -201,7 +243,7 @@
       (:body
         ,@body
         (:script :type "text/javascript" :src "/static/js/jquery-1.10.2.min.js")
-        (:script :type "text/javascript" :src "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js")
+        (:script :type "text/javascript" :src "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js")
         (:script :type "text/javascript" :src "/static/js/bootstrap/bootstrap.min.js")
         (:script :type "text/javascript" :src "/static/js/conditionizr.min.js")
         (:script :type "text/javascript" :src "/static/js/plugins/core/nicescroll/jquery.nicescroll.min.js")
@@ -209,7 +251,6 @@
         (:script :type "text/javascript" :src "/static/js/jquery.redshiftnetAdmin.js")
         (:script :type "text/javascript" :src "/static/js/plugins/ui/jgrowl/jquery.jgrowl.min.js")
         ;(:script :type "text/javascript" :src "/redshiftnet.js")
-        (:script :type "text/javascript" :src "/static/js/app.js")
         ,@(mapcar (lambda (file)
                     `(:script :type "text/javascript"
                               :src ,(format nil "~A" file)))
@@ -271,7 +312,8 @@
 ;; admin pages return nil unless inside an ssl vhost defrequest
 (defmacro %admin-auth-page ((title lpf) &body body)
   "Core auth-page template."
-  `(when (hunchentoot:ssl-p)
+  `(postmodern:with-connection (list ,*primary-db* ,*primary-db-user* ,*primary-db-pass* ,*primary-db-host*)
+    (when (hunchentoot:ssl-p)
      (if (null (hunchentoot:session-value 'token))
          (cond ((eql :post (hunchentoot:request-method*))
                 (let ((username (trim-or-nil (cl-who:escape-string (hunchentoot:post-parameter "username"))))
@@ -306,7 +348,7 @@
                                               :styles ,@(list admin-login-styles))
                         (show-all-messages)
                         (cl-who:with-html-output (hunchentoot::*standard-output*)
-                          (funcall ,lpf "/static/images/redshiftnet_text_logo.png")))))))))
+                          (funcall ,lpf "/static/images/redshiftnet_text_logo.png"))))))))))
 
 (defmacro admin-page ((title login-page-fun &key (styles nil) (scripts nil)) &body body)
   "Admin site page generator macro."
@@ -337,9 +379,46 @@
   (admin-page ("REDSHIFTNET Admin :: Dashboard" #'admin-login
                :styles admin-dashboard-styles
                :scripts admin-dashboard-scripts)
-     (admin-dashboard)
-     (cl-who:with-html-output (hunchentoot::*standard-output*)
-       (:p "A test paragraph, just to see what the deal is... and more... and..."))))
+    (admin-dashboard)
+    (cl-who:with-html-output (hunchentoot::*standard-output*)
+      (:p "A test paragraph, just to see what the deal is... and more... and..."))))
+
+(defrequest rsn-admin/webmail (:vhost vhost-admin)
+  (admin-page ("REDSHIFTNET Admin :: Webmail" #'admin-login
+               :styles admin-dashboard-styles
+               :scripts admin-dashboard-scripts)
+    (html-to-stout
+      (:div :id "heading" :class "page-header"
+        (:h1 (:i :class "icon20 i-envelop-2") " Webmail"))
+      (:div :class "row"
+        (:div :id "email" :class "col-lg-12"
+          (:div :class "email-bar")
+          (:div :class "email-nav well")
+          (:div :class "email-wrapper"))))))
+
+(defrequest rsn-admin/calendar (:vhost vhost-admin)
+  (admin-page ("REDSHIFTNET Admin :: Calendar" #'admin-login
+               :styles admin-calendar-styles
+               :scripts admin-calendar-scripts)
+    (html-to-stout
+      (:div :id "heading" :class "page-header"
+        (:h1 (:i :class "icon20 i-calendar") " Calendar"))
+      (:div :class "row"
+        (:div :class "col-lg-8"
+          (:div :class "panel plain"
+            (:div :class "panel-heading"
+              (:i :class "icon20 i-calendar-3")
+              (:h4 "Calendar")
+              (:a :href "#" :class "minimize"))
+            (:div :class "panel-body noPadding"
+              (:div :id "calendar"))))
+        (:div :class "col-lg-4"
+          (:div :class "page-header"
+            (:h4 "Events " (:small "drop events to calendar")))
+          (:div :id "external-events"
+            (:div :class "external-event" "Meeting with client")
+            (:div :class "external-event" "Telephone interview")
+            (:div :class "external-event" "Face-to-face")))))))
 
 ;; Edit page for database record
 (defun generate-edit-page-for-database-record (table-name record-id)
@@ -393,23 +472,25 @@
     (html-to-stout
     (loop for (x . y) in final-table-list
           do (htm
-               (:div :class "col-lg-8"
-                (:div :class "page-header"
-                  (:h3 (str (string-symbol-to-label x))))
-               (:table :class "table table-striped"
-                 (:thead
-                   (:tr (:th "Table Name")))
-                 (:tbody
-                 (loop for i in y
-                       do (loop for j in i
-                          do (progn
-                            (htm
-                            (:tr
-                              (:td
-                                (:i :class "icon20 i-table")
-                                "&nbsp;"
-                                (:a :href (str (format nil "/admin/tables/?name=~a" j))
-                                  (str (string-symbol-to-label j))))))))))))))))))
+               (:div :class "panel panel-default"
+                 (:div :class "panel-heading"
+                   (:div :class "icon" (:i :class "icon20 i-table-2"))
+                   (:h4 (str (string-symbol-to-label x)) (:small " app tables"))
+                   (:a :href "#" :class "minimize"))
+                 (:div :class "panel-body noPadding"
+                   (:table :class "table table-bordered"
+                     (:tbody
+                     (loop for i in y
+                           do (loop for j in i
+                              do (progn
+                                (htm
+                                (:tr
+                                  (:td
+                                    (:i :class "icon20 i-table")
+                                    "&nbsp;"
+                                    (:a :href (str (format nil "/admin/tables/?name=~a" j))
+                                      (str (string-symbol-to-label j)))))))))))))
+               (:div :class "clearfix" "&nbsp;")))))))
 
 ;; All Tables request
 ;; Accepts get-parameter 'name' to display specific table
@@ -425,6 +506,37 @@
           (:div :id "heading" :class "page-header"
             (:h1 (:i :class "icon20 i-dashboard") " All Tables"))
           (:div :class "row"
-            (output-all-database-tables-as-html))))))
+            (:div :class "col-lg-8"
+              (output-all-database-tables-as-html))
+            (:div :class "col-lg-4"
+              (:div :class "panel panel-default"
+                (:div :class "panel-heading"
+                  (:div :class "icon" (:i :class "icon20 i-power"))
+                  (:h4 "Recent activity")
+                  (:a :href "#" :class "minimize"))
+                (:div :class "panel-body noPadding"
+                  (:ul :class "recent-activity"
+                    (:li :class "item"
+                      (:span :class "icon green" (:i :class "icon16 i-user"))
+                      (:span :class "text"
+                        "user "
+                        (:a :href "#" "ManningP")
+                        " changed password on user "
+                        (:a :href "#" "NonnyA"))
+                      (:span :class "ago" "10 min ago"))
+                    (:li :class "item"
+                      (:span :class "icon blue" (:i :class "icon16 i-notification"))
+                      (:span :class "text"
+                        (:a :href "#" "5 callbacks")
+                        " were automatically closed")
+                      (:span :class "ago" "1 day ago"))
+                    (:li :class "item"
+                      (:span :class "icon red" (:i :class "icon16 i-key"))
+                      (:span :class "text"
+                        "user "
+                        (:a :href "#" "DarkoD")
+                        " has "
+                        (:a :href "#" "5 failed login attempts"))
+                      (:span :class "ago" "2 days ago")))))))))))
 
 ;; EOF
