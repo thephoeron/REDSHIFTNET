3d43b28d0facdce3c947b27dc6235482202d0efd
the Phoeron <thephoeron@me.com>
2013-11-09 18:57:47 -0500

    Update VHOST functionality to support solve for issue #2

--- redshiftnet.lisp
+++ redshiftnet.lisp
@@ -7,18 +7,15 @@
 
 ;;; REDSHIFTNET SERVER
 
-(defun rsn-start ()
+(defun rsn-start (&key (www-port 8080) (ssl-port 8090))
     "Server Start function for REDSHIFTNET"
-    ; (when *acc*
-    ;     (format t "REDSHIFTNET Already Running on ~W... Stopping First...~%" *acc*)
-    ;     (hunchentoot:stop *acc*)
-    ;     (setf *acc* nil)
-    ;     (format t "REDSHIFTNET Stopped successfully.~%"))
     (ensure-directories-exist *www-acc-log*)
     (ensure-directories-exist *www-msg-log*)
     (ensure-directories-exist *ssl-acc-log*)
     (ensure-directories-exist *ssl-msg-log*)
     ;(postmodern:connect-toplevel *primary-db* *primary-db-user* *primary-db-pass* *primary-db-host*)
+    (setf (port vhost-web) www-port
+          (port vhost-ssl) ssl-port)
     (hunchentoot:start vhost-web)
     (hunchentoot:start vhost-ssl)
     (format t "~%REDSHIFTNET Started and Running:~%       WEB: ~W~%       SSL: ~W" vhost-web vhost-ssl))
@@ -30,11 +27,11 @@
       (format t "~%REDSHIFTNET running on acceptors ~W and ~W" (hunchentoot:stop vhost-web) (hunchentoot:stop vhost-ssl)))
     (format t "~%REDSHIFTNET Stopped successfully..."))
 
-(defun rsn-restart ()
+(defun rsn-restart (&key (www-port 8080) (ssl-port 8090))
   "Restart Server function for REDSHIFTNET.  Caveat programmer: Assumes SBCL environment and Quicklisp installed."
   (rsn-stop)
   (ql:quickload "redshiftnet")
-  (rsn-start)
+  (rsn-start :www-port www-port :ssl-port ssl-port)
   (sb-ext:gc :full t)
   (format t "~%REDSHIFTNET Restarted successfully..."))
 
--- vhost.lisp
+++ vhost.lisp
@@ -11,44 +11,44 @@
 ;; http vhost
 (defclass vhost (hunchentoot:acceptor)
   ;; slots
-  ((dispatch-table
-    :initform '()
-    :accessor dispatch-table
-    :documentation "List of dispatch functions"))
+  ((dispatch-table :initform '() :accessor dispatch-table
+                   :documentation "List of dispatch functions")
+   (port :initarg :port :accessor port :documentation "Listener port"))
   ;; options
-  (:default-initargs                    ; default-initargs must be used
-   :address "127.0.0.1"))               ; because ACCEPTOR uses it
+  (:default-initargs
+   :address "127.0.0.1"
+   :port 8080))
 
 ;; https vhost
 (defclass ssl-vhost (hunchentoot:ssl-acceptor)
-	;; slots
-	((dispatch-table
-		:initform '()
-		:accessor dispatch-table
-		:documentation "List of SSL Dispatch functions"))
-	;; options
-	(:default-initargs
-	 :address "127.0.0.1"))
+  ;; slots
+  ((dispatch-table :initform '() :accessor dispatch-table
+                   :documentation "List of SSL Dispatch functions")
+   (port :initarg :port :accessor port :documentation "Listener port"))
+  ;; options
+  (:default-initargs
+   :address "127.0.0.1"
+   :port 8090))
 
 ;;; Specialise ACCEPTOR-DISPATCH-REQUEST for VHOSTs
 (defmethod hunchentoot:acceptor-dispatch-request ((vhost vhost) request)
   ;; try REQUEST on each dispatcher in turn
   (mapc (lambda (dispatcher)
-	  (let ((handler (funcall dispatcher request)))
-	    (when handler               ; Handler found. FUNCALL it and return result
-	      (return-from hunchentoot:acceptor-dispatch-request (funcall handler)))))
-	(dispatch-table vhost))
+      (let ((handler (funcall dispatcher request)))
+        (when handler               ; Handler found. FUNCALL it and return result
+          (return-from hunchentoot:acceptor-dispatch-request (funcall handler)))))
+    (dispatch-table vhost))
   (call-next-method))
 
 ;;; Specialize ACCEPTOR-DISPATCH-REQUEST for SSL-VHOSTs
 (defmethod hunchentoot:acceptor-dispatch-request ((ssl-vhost ssl-vhost) request)
-	;; try REQUEST on each dispatcher in turn
-	(mapc (lambda (dispatcher)
-		(let ((handler (funcall dispatcher request)))
-			(when handler
-				(return-from hunchentoot:acceptor-dispatch-request (funcall handler)))))
-		(dispatch-table ssl-vhost))
-	(call-next-method))
+    ;; try REQUEST on each dispatcher in turn
+    (mapc (lambda (dispatcher)
+        (let ((handler (funcall dispatcher request)))
+            (when handler
+                (return-from hunchentoot:acceptor-dispatch-request (funcall handler)))))
+        (dispatch-table ssl-vhost))
+    (call-next-method))
 
 ;;; ======================================================================
 ;;; Now all we need to do is test it
