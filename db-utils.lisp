b009f51f4e6d725c624572a6da1b50fc11ed9238
the Phoeron <thephoeron@me.com>
2014-01-02 22:29:41 -0500

    Minor code clean-up

--- db-utils.lisp
+++ db-utils.lisp
@@ -1,4 +1,5 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*- file: db-utils.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*-
+;;;file: db-utils.lisp
 
 ;;;; Copyright (c) 2012 -- 2013 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
--- scripts.lisp
+++ scripts.lisp
@@ -1,4 +1,5 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*- file: scripts.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*-
+;;;; file: scripts.lisp
 
 ;;;; Copyright (c) 2012 -- 2013 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
