2cfa96a64c3037cf2492737d7ddb329be234bf17
the Phoeron <thephoeron@me.com>
2014-01-10 11:04:17 -0500

    Update copyright statement on auth module, clean-up emacs mode line comment

--- auth/auth-sessions.lisp
+++ auth/auth-sessions.lisp
@@ -1,6 +1,7 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*- file: auth-sessions.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: REDSHIFTNET; Base: 10 -*-
+;;;; file: auth-sessions.lisp
 
-;;;; Copyright (c) 2012 -- 2013 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2012 -- 2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :redshiftnet)
