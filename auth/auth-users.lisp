732d13d8a486dbe4234fceff68b05cee0738cf5c
the Phoeron <thephoeron@me.com>
2014-02-20 00:48:57 -0500

    RSN-AUTH module: Add new CL-ISAAC powered salt function and update HARDENED-PASSWORD for SHA512 and new salt

--- auth/auth-users.lisp
+++ auth/auth-users.lisp
@@ -6,11 +6,19 @@
 
 (in-package :redshiftnet)
 
-;; PBKDF2--Sha256 Password salter and hasher.
+;; PBKDF2--Sha256 Password salter and hasher functions
+(defun make-isaac-salt ()
+  "Generate a cryptographic random 512-bit salt for HARDENED-PASSWORD using ISAAC-64 Algorithm."
+  (let* ((ctx (isaac:init-kernel-seed :is64 t))
+         (string-salt (format nil "~64,'0x" (isaac:rand-bits-64 ctx 512)))
+         (octet-salt (babel:string-to-octets string-salt)))
+    (return-from make-isaac-salt octet-salt)))
+
 (defun hardened-password (password)
+  "Ironclad-powered PBKDF2--SHA512 password hashing function hardened with CL-ISAAC."
   (let ((the-pass (babel:string-to-octets password))
-        (the-salt (ironclad:make-random-salt)))
-    (ironclad:pbkdf2-hash-password-to-combined-string the-pass :salt the-salt :digest :sha256 :iterations 10000)))
+        (the-salt (make-isaac-salt)))
+    (ironclad:pbkdf2-hash-password-to-combined-string the-pass :salt the-salt :digest :sha512 :iterations 10000)))
 
 ;; validate user and password. they must be active as well
 (defun validate-credentials (username password &key (user-table 'rsn-auth-user))
